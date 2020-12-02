53422e275381304fc9c1f0e3319a086b31f480c2
Kiszuriwalilibori <kiszuriwalilibori@poczta.onet.pl>
2020-12-02 09:32:57 +0100

    Updates

---
+++ precache-manifest.0082740156181647e705283b7bb303a0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0439821c308be8a687c04a3966e092f3",
+    "url": "/google_books_router_redux/index.html"
+  },
+  {
+    "revision": "ee9e18604fb88bf3a6fc",
+    "url": "/google_books_router_redux/static/css/main.8e749614.chunk.css"
+  },
+  {
+    "revision": "76f9806d9c72bd586131",
+    "url": "/google_books_router_redux/static/js/0.fa5c303d.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/google_books_router_redux/static/js/0.fa5c303d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d380efbbc11bb2608a66",
+    "url": "/google_books_router_redux/static/js/1.a7e65b07.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/google_books_router_redux/static/js/1.a7e65b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe44428515a901708d9e",
+    "url": "/google_books_router_redux/static/js/10.dec5a7f0.chunk.js"
+  },
+  {
+    "revision": "0b6441d893ed5808296f",
+    "url": "/google_books_router_redux/static/js/11.85927cb2.chunk.js"
+  },
+  {
+    "revision": "b4962710e4fce7a5806f",
+    "url": "/google_books_router_redux/static/js/12.50335eca.chunk.js"
+  },
+  {
+    "revision": "ffa36c83ca318a9b63b6",
+    "url": "/google_books_router_redux/static/js/13.b0831cfb.chunk.js"
+  },
+  {
+    "revision": "060df4da0e8e8a7b592b",
+    "url": "/google_books_router_redux/static/js/4.dc4305a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/google_books_router_redux/static/js/4.dc4305a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f70024e54d9498fd9e",
+    "url": "/google_books_router_redux/static/js/5.82ccd115.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/google_books_router_redux/static/js/5.82ccd115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d695c2aff3ec7e1f0abe",
+    "url": "/google_books_router_redux/static/js/6.d54543f2.chunk.js"
+  },
+  {
+    "revision": "79cf9e61bb506bfe5f5b",
+    "url": "/google_books_router_redux/static/js/7.88a11817.chunk.js"
+  },
+  {
+    "revision": "ffe6be93eba4f1ba6613",
+    "url": "/google_books_router_redux/static/js/8.a7dc89e5.chunk.js"
+  },
+  {
+    "revision": "0a980622511431a68cbd",
+    "url": "/google_books_router_redux/static/js/9.e776a62b.chunk.js"
+  },
+  {
+    "revision": "ee9e18604fb88bf3a6fc",
+    "url": "/google_books_router_redux/static/js/main.2d3b18c8.chunk.js"
+  },
+  {
+    "revision": "d6c0ccf934408cb43f3d",
+    "url": "/google_books_router_redux/static/js/runtime-main.46077ec4.js"
+  },
+  {
+    "revision": "a2a2b9d4046bb34ee571519042292066",
+    "url": "/google_books_router_redux/static/media/background.a2a2b9d4.jpg"
+  },
+  {
+    "revision": "97299f7022ab77721ee008264ec15fd9",
+    "url": "/google_books_router_redux/static/media/background_desktop.97299f70.jpg"
+  },
+  {
+    "revision": "777921f0b8bb6f1979b7d713c5a9dde9",
+    "url": "/google_books_router_redux/static/media/background_mobile.777921f0.jpg"
+  },
+  {
+    "revision": "2dfa325929a5dd79428e24bc1093e3a9",
+    "url": "/google_books_router_redux/static/media/background_small.2dfa3259.jpg"
+  },
+  {
+    "revision": "110bc94cf4b3665d35b15986fc877b66",
+    "url": "/google_books_router_redux/static/media/background_tablet.110bc94c.jpg"
+  }
+]);
