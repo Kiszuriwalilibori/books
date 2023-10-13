f7f16109f4418ea420f2ad98482f7a15db51e235
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2023-10-13 15:54:33 +0200

    Updates

--- static/js/720.31c0f2bf.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoogle_books_finder=self.webpackChunkgoogle_books_finder||[]).push([[720],{720:function(e,o,n){n.r(o),n.d(o,{NotFoundPage:function(){return a}});var r=n(2791),s=n(6710),i=n(7626),u=n(184),a=function(){return(0,u.jsx)(s.Bc,{errorMessage:"Nie znaleziono ksi\u0105\u017cek spe\u0142niaj\u0105cych podane kryteria"})};o.default=r.memo((0,i.jT)(a))}}]);
-//# sourceMappingURL=720.31c0f2bf.chunk.js.map
