47655b11416c142ae932bcb07e2778b05e58dbe1
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2024-02-06 11:27:14 +0100

    applied useDelayedCondition to loading indicator

--- src/components/LoadingIndicator/LoadingIndicator.tsx
+++ src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -2,6 +2,7 @@ import { connect } from "react-redux";
 
 import { ProgressIndicator, Container } from "./LoadingIndicator.styles";
 import { RootStateType } from "types";
+import useDelayedCondition from "hooks/useDelayedCondition";
 
 interface Props {
     isLoading: RootStateType["loading"]["isLoading"];
@@ -10,8 +11,10 @@ interface Props {
 const LoadingIndicator = (props: Props) => {
     const { isLoading, areDetailsLoading = false } = props;
 
-    if (!isLoading && !areDetailsLoading) return null;
+    const shouldRender = useDelayedCondition(isLoading || areDetailsLoading);
 
+    // if (!isLoading && !areDetailsLoading) return null;
+    if (!shouldRender) return null;
     return (
         <Container>
             <ProgressIndicator thickness={5} size={100} />
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,14 +1,15 @@
+import useDebouncedCallback from "./useDebouncedCallback";
+import useDelayedCondition from "./useDelayedCondition";
 import useDispatchAction from "./useDispatchAction";
 import useFavoriteBooks from "./useFavoriteBooks";
 import useFetchBooks from "./useFetchBooks";
+import useGoToShop from "./useGoToShop";
 import useHandleConnectionStatus from "./useHandleConnectionStatus";
 import useMessage from "./useMessage";
-import useGoToShop from "./useGoToShop";
+import usePersistDetailsURL from "./usePersistDetailsURL";
 
 import { useAppDispatch } from "./useAppDispatch";
 import { useTypedSelector } from "./useTypedSelector";
 import { useCreateDebouncedCallback } from "./useCreateDebouncedCallback";
-import useDebouncedCallback from "./useDebouncedCallback";
-import usePersistDetailsURL from "./usePersistDetailsURL";
 
-export { useAppDispatch, useCreateDebouncedCallback, useDebouncedCallback, useDispatchAction, useFavoriteBooks, useFetchBooks, useGoToShop, useHandleConnectionStatus, useMessage, usePersistDetailsURL, useTypedSelector };
+export { useAppDispatch, useCreateDebouncedCallback, useDebouncedCallback, useDelayedCondition, useDispatchAction, useFavoriteBooks, useFetchBooks, useGoToShop, useHandleConnectionStatus, useMessage, usePersistDetailsURL, useTypedSelector };
