da3e5229e8c61a55e44469c94b45907af616d651
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2024-09-13 18:41:00 +0200

    Updates

--- static/js/284.d201b120.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbooks=self.webpackChunkbooks||[]).push([[284],{9284:function(e,n,o){o.r(n),o.d(n,{NotFoundPage:function(){return i}});var t=o(2791),s=o(5552),r=o(4505),c=o(7689),u=o(3329),i=function(){var e=(0,c.s0)();return(0,t.useEffect)((function(){setTimeout((function(){e(-1)}),3e3)}),[]),(0,u.jsxs)(u.Fragment,{children:[(0,u.jsx)(s.sF,{}),(0,u.jsx)(s.Bc,{errorMessage:"Strona o adresie ".concat(decodeURIComponent(window.location.href)," nie istnieje")})]})};n.default=t.memo((0,r.jT)(i))}}]);
-//# sourceMappingURL=284.d201b120.chunk.js.map
--- static/js/720.fde711ef.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbooks=self.webpackChunkbooks||[]).push([[720],{720:function(e,n,s){s.r(n),s.d(n,{NotFoundPage:function(){return u}});var o=s(2791),r=s(5552),a=s(4505),t=s(3329),u=function(){return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)(r.sF,{}),(0,t.jsx)(r.Bc,{errorMessage:"Nie znaleziono ksi\u0105\u017cek spe\u0142niaj\u0105cych podane kryteria"})]})};n.default=o.memo((0,a.jT)(u))}}]);
-//# sourceMappingURL=720.fde711ef.chunk.js.map
