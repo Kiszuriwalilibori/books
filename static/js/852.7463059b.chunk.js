25bb627f41d69d4e81e24d544e5fb57a455b7e33
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2024-10-18 11:34:15 +0200

    Updates

--- static/js/852.7463059b.chunk.js
+++ static/js/852.7463059b.chunk.js
@@ -1,2 +1,2 @@
 !function(){"use strict";self.onmessage=function(s){var e=2*s.data;self.postMessage(e)}}();
-//# sourceMappingURL=416.e7d76123.chunk.js.map
+//# sourceMappingURL=852.7463059b.chunk.js.map
