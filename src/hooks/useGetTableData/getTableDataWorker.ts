4dab706f751badc64f208e85517b503e504d5c43
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2024-10-24 13:33:14 +0200

    settingcurrentpage in worker

--- src/hooks/useGetTableData/getTableDataWorker.ts
+++ src/hooks/useGetTableData/getTableDataWorker.ts
@@ -1,12 +1,16 @@
 /* eslint-disable no-restricted-globals */
 
+import getNumberOfPages from "js/BooksManager/tableHelpers/getNumberOfPages";
 import getSinglePageData from "js/BooksManager/tableHelpers/getSinglePageData";
 import sort from "js/BooksManager/tableHelpers/sort";
 import { GetTableDataParams } from "types";
 
 self.onmessage = (e: MessageEvent<GetTableDataParams>) => {
     const sortedBooks = e.data.currentSortColumn ? sort(e.data.books, e.data.isSortOrderDescending, e.data.currentSortColumn) : e.data.books;
-    const data = getSinglePageData(e.data.pageNumber, sortedBooks);
+    const singlePageData = getSinglePageData(e.data.pageNumber, sortedBooks);
+    const numberOfPages = getNumberOfPages(sortedBooks);
+    const currentPageNumber = e.data.pageNumber > numberOfPages ? numberOfPages : e.data.pageNumber;
+    const data = { singlePageData, currentPageNumber };
 
     self.postMessage(data);
 };
--- src/hooks/useGetTableData/useGetTableData.ts
+++ src/hooks/useGetTableData/useGetTableData.ts
@@ -1,24 +1,25 @@
 import { useMemo, useState, useEffect } from "react";
 import { shallowEqual, useSelector } from "react-redux";
-import { Books } from "types";
-
-import { useTypedSelector } from "hooks";
-
+import { Books, RootStateType } from "types";
+import { useDispatchAction, useTypedSelector } from "hooks";
 import { selectGetTableDataArgs } from "js/redux/selectors";
+import { useUpdateNumberOfPages } from "./useUpdateNumberOfPages";
+
 import getNumberOfPages from "js/BooksManager/tableHelpers/getNumberOfPages";
 
-import { useUpdateCurrentPageNumber } from "./useUpdateCurrentPageNumber";
-import { useUpdateNumberOfPages } from "./useUpdateNumberOfPages";
+interface TableDataWorkerMessageEvent {
+    singlePageData: Books;
+    currentPageNumber: RootStateType["books"]["currentPageNumber"];
+}
 
 const useGetTableData = () => {
     const [newBooks, setNewBooks] = useState<Books>([] as Books);
+    const { changePage } = useDispatchAction();
 
     const getTableDataWorker: Worker = useMemo(() => new Worker(new URL("./getTableDataWorker.ts", import.meta.url)), []);
     const books = useTypedSelector(state => state.books.books, shallowEqual);
 
     const args = useSelector(selectGetTableDataArgs);
-
-    useUpdateCurrentPageNumber(args.pageNumber, getNumberOfPages(books)); //todo prawdopodobnie już ni trzeba w getnumberofopages tego modyfikowac
     useUpdateNumberOfPages(getNumberOfPages(books));
     const params = { ...args, books };
 
@@ -30,8 +31,10 @@ const useGetTableData = () => {
 
     useEffect(() => {
         if (window.Worker) {
-            getTableDataWorker.onmessage = (e: MessageEvent<Books>) => {
-                setNewBooks((prev: any) => e.data);
+            getTableDataWorker.onmessage = (e: MessageEvent<TableDataWorkerMessageEvent>) => {
+                const { singlePageData, currentPageNumber } = { ...e.data };
+                setNewBooks((prev: Books) => singlePageData);
+                changePage(currentPageNumber);
             };
         }
     }, [getTableDataWorker]);
