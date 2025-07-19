c8b220059549cf55590860a0538a6efdb9e08bfe
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2025-07-19 17:47:43 +0200

    configured i18next

---
+++ src/i18n/config.ts
@@ -0,0 +1,32 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { InitOptions } from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const i18nConfig: InitOptions = {
+    fallbackLng: "en",
+    resources: {
+        en: {
+            translation: {},
+        },
+        pl: {
+            translation: {},
+        },
+    },
+    ns: ["translation"],
+    defaultNS: "translation",
+    detection: {
+        order: ["localStorage", "navigator"],
+        caches: ["localStorage"],
+        lookupLocalStorage: "i18nextLng",
+    },
+};
+
+i18n.use(LanguageDetector).use(initReactI18next).init(i18nConfig);
+
+i18n.addResourceBundle("en", "translation", require("./locales/en/translation.json"), true, true);
+i18n.addResourceBundle("pl", "translation", require("./locales/pl/translation.json"), true, true);
+
+i18n.languages = ["en", "pl"];
+
+export default i18n;
